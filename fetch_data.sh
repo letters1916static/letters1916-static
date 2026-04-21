abd6fe4e524a996e441a3d67971430f6c96ee023
Arnoud <arnoud.wils@gmail.com>
2026-04-21 10:16:47 +0200

    Updated fetch_data.sh to add index attributes

--- fetch_data.sh
+++ fetch_data.sh
@@ -12,3 +12,6 @@ rm -rf ./letters1916-data-main
 
 echo "fetch imprint"
 ./shellscripts/dl_imprint.sh
+
+uv run add-attributes -g "./data/editions/*.xml" -b "https://id.acdh.oeaw.ac.at/letters1916"
+uv run denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/*.xml" -m ".//*[@key]/@key" -x ".//tei:title[@type='main']/text()"
